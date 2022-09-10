fa174a6a9aece921daafcdef4354f4b4a6609dac
Jan <me@janderedev.xyz>
2022-09-10 21:30:53 +0200

    make example code work with the latest plugin api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-(state: any) => ({
-    onClient: (client: import('revolt.js').Client) => {
-        console.log('Received Client:', client.user.username);
-    },
-    onUnload: () => console.log('Plugin unloaded'),
-});
+() => {
+    // Write your code here.
+
+    const client: import('revolt.js').Client = (window as any).controllers.client.getReadyClient();
+    console.log('Received Client:', client.user.username);
+
+    return ({
+        onUnload: () => {
+            // This runs when the plugin is disabled or unloaded.
+            console.log('Plugin unloaded');
+        }
+    });
+};
