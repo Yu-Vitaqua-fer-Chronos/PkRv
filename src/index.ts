595ba22ab131b646e5dc4c02d487f3d9eb8b5811
Mythical Forest Collective <Random.Access.Exomemory@gmail.com>
2022-11-02 20:53:57 +0000

    Current broken code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 () => {
     const PK_TOKEN = "QUEEEEEEEEEEEEER"
     const API_URL = "https://api.pluralkit.me/v2/"
-    const MEMBERS_URL = "systems/@me/members"
+    const SYSTEM_URL = "systems/@me/"
+    const MEMBERS_URL = "members"
+
+   function getPluralKitSystemTag() {
+        fetch(API_URL+SYSTEM_URL, {
+            headers: {'Authorization': PK_TOKEN}
+        }).then(res => res.json()).then(sys => {return sys.tag});
+    }
+
+    let SYSTEM_TAG = getPluralKitSystemTag();
+    var latch_mode: boolean = false;
+    var last_proxied_member: any = null;
 
     async function getPluralKitMembers() {
-        console.log("[PkRv] Getting member list")
-        var res = await fetch(API_URL+MEMBERS_URL, {
+        var res = await fetch(API_URL+SYSTEM_URL+MEMBERS_URL, {
             headers: {'Authorization': PK_TOKEN}
         })
 
         let data = await res.json()
-        console.log("[PkRv] Recieved JSON: "+data)
         return data
     }
 
@@ -29,7 +38,7 @@
 
         // Assigning right back on channel is required to be able to reuse it.
         channel['origSend'] = channel.sendMessage;
-        channel.sendMessage = async function(data: string | (Omit<{ attachments?: null | string[]; content?: null | string; embeds?: null | { colour?: null | string; description?: null | string; icon_url?: null | string; media?: null | string; title?: null | string; url?: null | string }[]; interactions?: null | { reactions?: null | string[]; restrict_reactions?: boolean }; masquerade?: null | { avatar?: null | string; colour?: null | string; name?: null | string }; nonce?: null | string; replies?: null | { id: string; mention: boolean }[] }, "nonce"> & { nonce?: string })): Promise<any> {
+        channel.sendMessage = async function(data: string | (Omit<{ attachments?: null | string[]; content?: null | string; embeds?: null | { colour?: null | string; description?: null | string; icon_url?: null | string; media?: null | string; title?: null | string; url?: null | string }[]; interactions?: null | { reactions?: null | string[]; restrict_reactions?: boolean }; masquerade?: null | { avatar?: null | string; colour?: null | string; name?: null | string }; nonce?: null | string; replies?: null | { id: string; mention: boolean }[] }, "nonce"> & { nonce?: string })): Promise<import('revolt.js').Message> {
             var msgData: any = {}
 
             if (data['content']) {
@@ -38,47 +47,57 @@
                 msgData.content = data;
             }
 
-            var m: any = null;
+            if (msgData.content == "pk;ap latch") {
+                latch_mode = !latch_mode
+                if (!latch_mode) {
+                    last_proxied_member = null;
+                }
+                msgData.content = "Latch set to "+latch_mode
+                return await this.origSend(msgData);
+            }
+
+            var m: any = last_proxied_member;
             var proxy_tag: any = null;
 
             outer: for (let member of members) {
-                console.log("[PkRv] On member "+member.name)
                 for (let proxy of member.proxy_tags) {
                     if (msgData.content.startsWith(proxy.prefix || "") && msgData.content.endsWith(proxy.suffix || "")) {
                         m = member;
                         proxy_tag = proxy;
+                        if (latch_mode) {
+                            last_proxied_member = m
+                        }
                         break outer;
                     }
                 }
             }
 
-            if(m) {
+            if (m) {
                 msgData.masquerade = {}
 
-                var start = proxy_tag.prefix?.length ?? 0
-                var end = msgData.content.length - (proxy_tag.suffix?.length ?? 0)
+                if (proxy_tag) {
+                    var start = proxy_tag.prefix?.length ?? 0
+                    var end = msgData.content.length - (proxy_tag.suffix?.length ?? 0)
 
-                msgData.content = msgData.content.slice(start, end);
+                    msgData.content = msgData.content.slice(start, end);
+                }
 
                 // Set the masquerade object.
-                msgData.masquerade.name = m.display_name || m.name
+                msgData.masquerade.name = `${(m.display_name || m.name)} ${SYSTEM_TAG}`
                 msgData.masquerade.avatar = m.avatar_url
             }
 
-            await this.origSend(msgData);
+            console.log(`[PkRv] ${JSON.stringify(msgData)}`)
+            return await this.origSend(msgData);
         }
     }
 
     var client: import('revolt.js').Client = client = (window as any).controllers.client.getReadyClient();
-    console.log('[PkRv] Received Client:', client.user.username);
 
     client.on("message", async (message) => {
         if (message.author == client.user && !monkeyPatchedChannels.has(message.channel)) {
             monkeypatchChannelSend(message.channel);
 
-            // There is *no* reason to do this after monkey patching.
-            // Do not set this outside of monkey patching as this *will*
-            // cause an endless loop until ratelimiting.
             await message.channel!.sendMessage(message.content);
             await message.delete()
         }
@@ -86,7 +105,10 @@
 
     return ({
         onUnload: () => {
-            console.log('[PkRv] Plugin unloaded');
+            for (let channel of monkeyPatchedChannels.keys()) {
+                channel.sendMessage = channel['origSend'];
+            }
+            console.log('[PkRv] Plugin unloaded!');
         }
     });
 };
